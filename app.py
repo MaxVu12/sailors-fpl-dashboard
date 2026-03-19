592fc48672a7843e720c561d836bf3557a072bf7
Max Vu <maxvu12@gmail.com>
2026-03-19 16:13:19 -0400

    fix horizontal spacing

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ if st.button('Fetch Live Standings'):
             # Display the table
             st.dataframe(
                 styled_df, 
-                use_container_width=True,
+                width='content',
                 height='content', 
                 hide_index=True
             )
